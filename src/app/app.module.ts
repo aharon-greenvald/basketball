c41d1264ac0eb84f76e7ebd9524a4a4e4a94c262
greenvald <greenvald@gmail>
2020-09-17 19:04:47 +0300

    th2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// MDB Angular Pro
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/main-page/main-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+  l ="../../../assets/aron.jpg"
+list = [
+  {name:'aron',made:0,missed:0},
+  {name:'moti',made:35,missed:0},
+  {name:'shlomo',made:23,missed:0},
+]
+a =''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addCounter(player){
+    player.made++
+    console.log(player);
+    
+  }
+  missesCounter(player){
+    player.missed++
+    console.log(player);
+    
+  }
+
+}
