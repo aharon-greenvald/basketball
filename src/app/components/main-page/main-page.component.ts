3846de123584032e6e002f3f7c4f24ddbc54887a
greenvald <greenvald@gmail>
2020-09-21 18:36:00 +0300

    4

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from 'src/shared/services/list.service';
 
 
 @Component({
@@ -8,17 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
   l ="../../../assets/aron.jpg"
-list = [
-  {name:'aron',made:0,missed:0},
-  {name:'moti',made:35,missed:0},
-  {name:'shlomo',made:23,missed:0},
-  {name:'avia',made:23,missed:0},
-  {name:'nisim',made:23,missed:0},
-  {name:'israel',made:23,missed:0},
-  {name:'yakov',made:23,missed:0},
-]
+
 a =''
-  constructor() { }
+  constructor(private srv:ListService) { }
 
   ngOnInit(): void {
   }
