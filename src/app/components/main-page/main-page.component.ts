6380f37b322249a8756fbf278dd148c113169ec2
greenvald <greenvald@gmail>
2020-09-21 18:17:23 +0300

    home

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -12,6 +12,10 @@ list = [
   {name:'aron',made:0,missed:0},
   {name:'moti',made:35,missed:0},
   {name:'shlomo',made:23,missed:0},
+  {name:'avia',made:23,missed:0},
+  {name:'nisim',made:23,missed:0},
+  {name:'israel',made:23,missed:0},
+  {name:'yakov',made:23,missed:0},
 ]
 a =''
   constructor() { }
